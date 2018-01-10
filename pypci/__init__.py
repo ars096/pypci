840230f528283177e19402a82900e359eb7d9505
Atsushi Nishimura <ars096@gmail.com>
2018-01-10 18:21:03 +0900

    apply to test

--- pypci/__init__.py
+++ pypci/__init__.py
@@ -1,7 +1,10 @@
 
 __version__ = '0.1.1'
 
+try:
+    from .pypci import lspci
+    from .pypci import read
+    from .pypci import write
 
-from .pypci import lspci
-from .pypci import read
-from .pypci import write
+except:
+    pass
