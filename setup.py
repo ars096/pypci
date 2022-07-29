bed7bad7b317439925c975511a8a991654b8b410
KaoruNishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-07-29 04:31:27 +0000

    Add description on PyPI page

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 
 import setuptools
+from pathlib import Path
+
+long_description = (Path(__file__).parent / "README.md").read_text()
 
 setuptools.setup(
     name = 'pypci',
     version = "1.0.4",
     description = 'PCI driver for python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url = 'https://github.com/ars096/pypci',
     author = 'Atsushi Nishimura',
     author_email = 'ars096@gmail.com',
