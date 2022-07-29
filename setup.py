76ed239a93b257fddb2c53216913e0faf065213c
KaoruNishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-07-29 04:42:23 +0000

    Fix project version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setuptools.setup(
     name = 'pypci',
-    version = "1.0.4",
+    version = "0.1.4",
     description = 'PCI driver for python',
     long_description=long_description,
     long_description_content_type="text/markdown",
